a52577a44d95dd2d2aa0f4ba050aed82d15edfac
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-01-23 21:33:16 +0100

    Add Azure Identity client sample

---
+++ azure-auth-app-sample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace azure_auth_app_sample;
+
+class Program
+{
+    private const string _clientId = "APPLICATION_CLIENT_ID";
+    private const string _tenantId = "DIRECTORY_TENANT_ID";
+
+    public static async Task Main(string[] args)
+    {
+        var app = PublicClientApplicationBuilder
+            .Create(_clientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+        string[] scopes = { "user.read" };
+        AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+        Console.WriteLine($"Token:\t{result.AccessToken}");
+    }
+}
