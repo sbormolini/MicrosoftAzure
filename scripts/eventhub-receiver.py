76f77a00372f527edfa7cb2e3e0da421259ab226
Sandro Bormolini <86348794+sbormolini@users.noreply.github.com>
2022-06-22 10:23:36 +0200

    Update eventhub-receiver.py

--- scripts/eventhub-receiver.py
+++ scripts/eventhub-receiver.py
@@ -14,13 +14,13 @@ async def on_event(partition_context, event):
 async def main():
     # Create an Azure blob checkpoint store to store the checkpoints.
     checkpoint_store = BlobCheckpointStore.from_connection_string(
-        "DefaultEndpointsProtocol=https;AccountName=rgbosastraeuschndevb735;AccountKey=aC/V12Zsru1tdJogead7K8Lmyi9fnGmcDC8Nek4HW1/dRM1se86/Ekl45BDDgcOSlXFuo2J4dtf/ETtWgc2arQ==;EndpointSuffix=core.windows.net", 
+        "DefaultEndpointsProtocol=https;AccountName=<name>;AccountKey=<key>;EndpointSuffix=core.windows.net", 
         "eventhub-checkpoint-store"
     )
 
     # Create a consumer client for the event hub.
     client = EventHubConsumerClient.from_connection_string(
-        "Endpoint=sb://testbos1.servicebus.windows.net/;SharedAccessKeyName=send-listen-hub;SharedAccessKey=zLmXBQrZgvze8MDVh7fPMEXYvuaZ3ifh0hMf7M0/Ja4=;EntityPath=testhub1", 
+        "Endpoint=sb://testbos1.servicebus.windows.net/;SharedAccessKeyName=send-listen-hub;SharedAccessKey=<key>;EntityPath=testhub1", 
         consumer_group="$Default", 
         eventhub_name="testhub1", 
         checkpoint_store=checkpoint_store
