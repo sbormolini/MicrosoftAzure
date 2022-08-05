947322f2c80ab8379778d1572a398a6e361fb3f7
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-08-05 22:58:13 +0200

    Added function tests

--- azure-functions-psmodule-sample/Modules/AzFunc.Helper/tests/AzFunc.Helper.Tests.ps1
+++ azure-functions-psmodule-sample/Modules/AzFunc.Helper/tests/AzFunc.Helper.Tests.ps1
@@ -1,5 +1,18 @@
-$ModuleManifestName = 'AzFunc.Helper.psd1'
-$ModuleManifestPath = (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath $ModuleManifestName)
+BeforeAll {
+    $ModuleManifestName = 'AzFunc.Helper.psd1'
+    $ModuleName = 'AzFunc.Helper.psm1'
+    if ($PSScriptRoot -eq $null)
+    {
+        # dev test path
+        $ModuleManifestPath = "..\$ModuleManifestName"
+        $ModulePath = "..\$ModuleName"
+    }
+    else
+    {
+        $ModuleManifestPath = (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath $ModuleManifestName)
+        $ModulePath = (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath $ModuleName)
+    }
+}
 
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
@@ -7,5 +20,16 @@ Describe 'Module Manifest Tests' {
         $manifest | Should -Not -BeNullOrEmpty
         $manifest.Name | Should -Be "AzFunc.Helper"
         $manifest.ModuleType | Should -Be "Script"
+    }
+}
+
+Describe 'Module Function Tests' {
+    BeforeAll {
+        Import-Module $ModulePath -Force
+    }
+    It 'Passes NewRandomPassword Length 12' {
+        $password = New-RandomPassword -Length 12
+        $password  | Should -Not -BeNullOrEmpty
+        $password.Length | Should -Be 12
     }
 }
