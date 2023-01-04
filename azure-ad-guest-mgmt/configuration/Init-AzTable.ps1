ede91f6b5bf78ab239a93dfe8b7ed7af2e9b3764
Sandro Bormolini <sandro.bormolini@outlook.com>
2023-01-04 22:40:25 +0100

    Add Az Table sample

---
+++ azure-ad-guest-mgmt/configuration/Init-AzTable.ps1
@@ -0,0 +1,27 @@
+#requires -Modules AzTable
+
+Import-Module -Name AzTable
+
+$partitionKey1 = "partition1"
+$partitionKey2 = "partition2"
+
+# add four rows 
+Add-AzTableRow `
+    -table $cloudTable `
+    -partitionKey $partitionKey1 `
+    -rowKey ("CA") -property @{"username"="Chris";"userid"=1}
+
+Add-AzTableRow `
+    -table $cloudTable `
+    -partitionKey $partitionKey2 `
+    -rowKey ("NM") -property @{"username"="Jessie";"userid"=2}
+
+Add-AzTableRow `
+    -table $cloudTable `
+    -partitionKey $partitionKey1 `
+    -rowKey ("WA") -property @{"username"="Christine";"userid"=3}
+
+Add-AzTableRow `
+    -table $cloudTable `
+    -partitionKey $partitionKey2 `
+    -rowKey ("TX") -property @{"username"="Steven";"userid"=4}
