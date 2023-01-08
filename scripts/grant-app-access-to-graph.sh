fa69ea8821cded7fe6db7bcf14fce8a1baf01d00
Sandro Bormolini <sandro.bormolini@outlook.com>
2023-01-08 14:52:56 +0100

    Add graph grant script

---
+++ scripts/grant-app-access-to-graph.sh
@@ -0,0 +1,15 @@
+az login
+
+webAppName="<WebAppName>"
+
+spId=$(az resource list -n $webAppName --query [*].identity.principalId --out tsv)
+
+graphResourceId=$(az ad sp list --display-name "Microsoft Graph" --query [0].id --out tsv)
+
+appRoleId=$(az ad sp list --display-name "Microsoft Graph" --query "[0].appRoles[?value=='User.Read.All' && contains(allowedMemberTypes, 'Application')].id" --output tsv)
+
+uri=https://graph.microsoft.com/v1.0/servicePrincipals/$spId/appRoleAssignments
+
+body="{'principalId':'$spId','resourceId':'$graphResourceId','appRoleId':'$appRoleId'}"
+
+az rest --method post --uri $uri --body $body --headers "Content-Type=application/json"
